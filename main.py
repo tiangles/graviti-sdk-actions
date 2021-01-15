4d82a0f44db3621e78d1e87b8b325dbae8320e1b
john.zhao <john.zhao@graviti.cn>
2021-01-15 09:38:56 +0800

    update code for easy test

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import logging
-
-from graviti import GAS
+import time
 
 logger = logging.getLogger()
-
-ACCESS_KEY = 'Accesskey-b5df9e1e22866b882dc146e5ba0f5645'
 
 
 def main():
     logger.info('application started.')
-    try:
-        gas = GAS(ACCESS_KEY)
-        datasets = gas.list_datasets()
-        for item in datasets:
-            logger.info(f'get dataset item {item}.')
-    except Exception as e:
-        logger.exception(f'script raise exception: {e}.')
-    finally:
-        logger.info('application finished.')
+    for i in range(40):
+        logger.info(f'\toutput with index {i}')
+        time.sleep(2000)
+    logger.info('application finished.')
 
 
 if __name__ == '__main__':
