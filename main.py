213288e0a7a7223cca884fa6f8f2d510c5d6a85a
john.zhao <john.zhao@graviti.cn>
2021-01-15 14:10:29 +0800

    update code

--- main.py
+++ main.py
@@ -4,20 +4,25 @@ from graviti import GAS
 
 logger = logging.getLogger()
 
-ACCESS_KEY = 'Accesskey-b5df9e1e22866b882dc146e5ba0f5645'
+ACCESS_KEY = 'Accesskey-b282d0e49157142b76ea3105b56066f0'
+DATASET_NAME = 'mint desktop background images'
 
 
 def main():
-    logger.info('application started.')
+    # logger.info('application started.')
     try:
         gas = GAS(ACCESS_KEY)
-        datasets = gas.list_datasets()
-        for item in datasets:
-            logger.info(f'get dataset item {item}.')
+        dataset_client = gas.get_dataset(DATASET_NAME)
+        segment = dataset_client.get_segment_object()
+        logger.info(f'list images in dataset segment')
+        for data in segment:
+            logger.info(data.remote_path)
+
     except Exception as e:
         logger.exception(f'script raise exception: {e}.')
-    finally:
-        logger.info('application finished.')
+
+    # finally:
+    #     logger.info('application finished.')
 
 
 if __name__ == '__main__':
